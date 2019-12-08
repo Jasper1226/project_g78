99c726780dfb475fec9efb39f6201ce9c24c9e22
Jie Xue <jx2401@columbia.edu>
2019-12-08 23:32:45 +0000

    Update views.py

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+class MapView(TemplateView):
+    """ View for performing the  sightings' map"""
+
+    template_name = 'squirrel/map.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['sightings'] = models.Sighting.objects.all()
+        return ctx
