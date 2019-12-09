12f8b97bbfd8993cf2e53094a39e6cf88975a3af
Xin Li <xslixin2007@gmail.com>
2019-12-09 23:25:33 +0000

    change the map function

--- map/views.py
+++ map/views.py
@@ -4,6 +4,6 @@ from sightings.models import Sighting
 
 def sighting_map(request):
     return render(request, 'map/map.html', {
-        'sightings': Sighting.objects.all()
+        'sightings': Sighting.objects.all()[:100]
     })
 # Create your views here.
