abfbc466a270825c12943ca9a51b754bbd09f0f0
Jie Xue <jx2401@columbia.edu>
2019-12-09 21:33:20 +0000

    "Little Change"

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('sightings/', views.list_sightings),
+    path('sightings', views.list_sightings),
     path('sightings/stat', views.stat_sighting),
     path('sightings/add', views.add_sighting),
     path('sightings/<str:unique_squirrel_id>', views.edit_sighting),
