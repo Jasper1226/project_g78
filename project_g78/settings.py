a08f3a4a0f3a25fd3b3dec6696139030fb7898b3
Jie Xue <jx2401@columbia.edu>
2019-12-09 20:43:45 +0000

    "Update Totally, make new apps!"

--- project_g78/settings.py
+++ project_g78/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'squirrel.apps.SquirrelConfig',
+    'sightings.apps.SightingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project_g78/urls.py
+++ project_g78/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('squirrel/', include('squirrel.urls')),
+    path('map/', include('map.urls')),
 ]
